0e2fedd73a78bbe4adb546a35cfd136e2dee81db
FrizakN1 <frizak2017@mail.ru>
2025-05-19 21:44:01 +0500

    Добавлена проверка существования папки logs

--- utils/logger.go
+++ utils/logger.go
@@ -14,16 +14,19 @@ type Logger interface {
 func InitLogger() Logger {
 	logDir := "logs"
 
-	if err := os.Mkdir(logDir, os.ModePerm); err != nil {
-		fmt.Println("ошибка создания папки logs:", err)
-		return log.Default()
+	_, err := os.Stat(logDir)
+	if os.IsNotExist(err) {
+		if err = os.Mkdir(logDir, os.ModePerm); err != nil {
+			log.Fatalf("ошибка создания папки logs: %e\n", err)
+			return log.Default()
+		}
 	}
 
 	currentDate := time.Now().String()[0:10]
 
 	loggerFile, e := os.OpenFile(fmt.Sprintf("%s/%s.log", logDir, currentDate), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if e != nil {
-		fmt.Println(e)
+		log.Fatalln(e)
 		return log.Default()
 	}
 
