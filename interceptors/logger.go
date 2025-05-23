a545a5858b9529cffa303ee2e048a5586a883443
FrizakN1 <frizak2017@mail.ru>
2025-05-23 12:16:01 +0500

    Добавлен интерцептор дял логирования

--- interceptors/logger.go
+++ interceptors/logger.go
@@ -4,22 +4,23 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	"user-service/kafka"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func LoggingInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		resp, err := handler(ctx, req)
-		fmt.Println(123)
-		status := "OK"
+
+		statusCode := codes.OK
 		if err != nil {
 			fmt.Println(err)
-			//status = status.Code(err).String()
+			st, _ := status.FromError(err)
+			statusCode = st.Code()
 		}
 
-		message := fmt.Sprintf("method=%s status=%s", info.FullMethod, status)
+		message := fmt.Sprintf("method=%s status=%s", info.FullMethod, statusCode)
 		fmt.Println(message)
-		kafka.LogToKafka(ctx, message)
 
 		return resp, err
 	}
